6c3400513e942092533dd9d79bc3f273017f1fe3
Jhon Robert Quintero <jrquinte@hotmail.com>
2021-04-11 17:01:33 -0500

    Chapter 14.3 Version: Java Concurrency and Multithreading. Modified to Simulate concurrent callers

--- src/labs/pm/app/Shop.java
+++ src/labs/pm/app/Shop.java
@@ -16,7 +16,22 @@
  */
 package labs.pm.app;
 
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import labs.pm.data.Product;
 import labs.pm.data.ProductManager;
+import labs.pm.data.Rating;
 
 /**
  * {@code Shop} class represents an application that manages Products
@@ -28,13 +43,57 @@ import labs.pm.data.ProductManager;
 public class Shop {
 
     /**
-     * @param args the command line arguments
+     * @param args the command line arguments 14.1 Make ProductManager class as
+     * Immutable as possible to prevent accidental collisions between concurrent
+     * callers. 14.3 Code in the main method of a Shop class that simulates
+     * several concurrent callers that share a single instance of the
+     * ProductManager
+     *
+     *
      */
-    public static void main(String[] args) {
-
+    public static void main(String[] args) {        
         ProductManager pm = ProductManager.getInstance(); //ProductManager();
-        pm.printProductReport(101,"en-GB");
-        pm.printProductReport(103,"ru-RU");
+        
+        AtomicInteger clientCount = new AtomicInteger(0);
+        //Callable object to represent a concurrent client
+        Callable<String> client = () -> {
+            String clientId = "Client " + clientCount.incrementAndGet();
+            String threadName = Thread.currentThread().getName();
+            int productId = ThreadLocalRandom.current().nextInt(63) + 101;
+            String languageTag = ProductManager.getSupportedLocale()
+                    .stream()
+                    .skip(ThreadLocalRandom.current().nextInt(4))
+                    .findFirst().get();
+            StringBuilder log = new StringBuilder();
+            log.append(clientId + " " + threadName + "\n-\tstart of log\t-\n");
+            log.append(pm.getDiscounts(languageTag)
+                    .entrySet()
+                    .stream()
+                    .map(entry -> entry.getKey() + "\t" + entry.getValue())
+                    .collect(Collectors.joining("\n")));            
+            Product product = pm.reviewProduct(productId, Rating.FOUR_STAR, "Yet another review");
+            log.append((product != null) ? "\nProduct " + productId + " reviewed\n" : "\nProduct " + productId + "not reviewed\n");
+            log.append(clientId + " generated report for " + productId + " product");
+            log.append("\n-\tend of log\t-\n");
+            return log.toString();
+        };
+        //Prepare to invoke five concurrent client Callable objects
+        List<Callable<String>> clients = Stream.generate(() -> client).limit(5).collect(Collectors.toList());
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        try {
+            List<Future<String>> results = executorService.invokeAll(clients);
+            executorService.shutdown();
+            results.stream().forEach((Future<String> result) -> {
+                try {
+                    System.out.println(result.get());
+                } catch (InterruptedException | ExecutionException ex) {
+                    Logger.getLogger(Shop.class.getName()).log(Level.SEVERE, "Error retrieving client log", ex);
+                }
+            });
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Shop.class.getName()).log(Level.SEVERE, "Error invoking clients", ex);
+        }
+        
     }
-
+    
 }
